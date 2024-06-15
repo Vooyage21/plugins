c257be6e5f02042f10180fe88a857db403a9417d
Dante <148047531+Vooyage21@users.noreply.github.com>
2024-06-16 04:59:40 +0700

    update plugins/gitinfo.py

--- plugins/gitinfo.py
+++ plugins/gitinfo.py
@@ -8,7 +8,7 @@ from VIPMUSIC import app
 @app.on_message(filters.command(["github", "git"]))
 async def github(_, message):
     if len(message.command) != 2:
-        await message.reply_text("/git VIP-BOY-OP")
+        await message.reply_text("/git Music Bot")
         return
 
     username = message.text.split(None, 1)[1]
