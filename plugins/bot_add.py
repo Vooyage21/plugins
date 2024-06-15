d5a22069e4f4104baff3bffdf1e597e30cde698c
Dante <148047531+Vooyage21@users.noreply.github.com>
2024-06-16 05:04:11 +0700

    update plugins/bot_add.py

--- plugins/bot_add.py
+++ plugins/bot_add.py
@@ -1,66 +1,40 @@
 import random
-
+from pyrogram import Client
+from pyrogram.types import Message
 from pyrogram import filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto, InputMediaVideo, Message
 from config import LOGGER_ID as LOG_GROUP_ID
-from VIPMUSIC import app
-from VIPMUSIC.utils.database import add_served_chat, get_assistant
+from VIPMUSIC import app  
+from VIPMUSIC.core.userbot import Userbot
+from VIPMUSIC.utils.database import delete_served_chat
+from VIPMUSIC.utils.database import get_assistant
+
 
 photo = [
-    "https://telegra.ph/file/1949480f01355b4e87d26.jpg",
-    "https://telegra.ph/file/3ef2cc0ad2bc548bafb30.jpg",
-    "https://telegra.ph/file/a7d663cd2de689b811729.jpg",
-    "https://telegra.ph/file/6f19dc23847f5b005e922.jpg",
-    "https://telegra.ph/file/2973150dd62fd27a3a6ba.jpg",
+    "https://mallucampaign.in/images/img_1714463551.jpg",
+    "https://mallucampaign.in/images/img_1713935923.jpg",
 ]
 
-from strings.__init__ import LOGGERS
-
-
-@app.on_message(filters.new_chat_members, group=-10)
-async def join_watcher(_, message):
+@app.on_message(filters.new_chat_members, group=2)
+async def join_watcher(_, message):    
     try:
         userbot = await get_assistant(message.chat.id)
         chat = message.chat
         for members in message.new_chat_members:
             if members.id == app.id:
                 count = await app.get_chat_members_count(chat.id)
-                username = (
-                    message.chat.username if message.chat.username else "𝐏ʀɪᴠᴀᴛᴇ 𝐆ʀᴏᴜᴘ"
-                )
+                username = message.chat.username if message.chat.username else "Private Group"
                 msg = (
-                    f"**📝𝐌ᴜsɪᴄ 𝐁ᴏᴛ 𝐀ᴅᴅᴇᴅ 𝐈ɴ 𝐀 #𝐍ᴇᴡ_𝐆ʀᴏᴜᴘ**\n\n"
-                    f"**📌𝐂ʜᴀᴛ 𝐍ᴀᴍᴇ:** {message.chat.title}\n"
-                    f"**🍂𝐂ʜᴀᴛ 𝐈ᴅ:** `{message.chat.id}`\n"
-                    f"**🔐𝐂ʜᴀᴛ 𝐔sᴇʀɴᴀᴍᴇ:** @{username}\n"
-                    f"**📈𝐆ʀᴏᴜᴘ 𝐌ᴇᴍʙᴇʀs:** {count}\n"
-                    f"**🤔𝐀ᴅᴅᴇᴅ 𝐁ʏ:** {message.from_user.mention}"
+                    f"**📝ᴍᴜsɪᴄ ʙᴏᴛ ᴀᴅᴅ ᴏɴ #ɴᴇᴡ_ɢʀᴏᴜᴘ**\n\n"
+                    f"**📌Chat Name:** {message.chat.title}\n"
+                    f"**🍂Chat ID:** {message.chat.id}\n"
+                    f"**🔐Chat Username:** @{username}\n"
+                    f"**📈Group Members:** {count}\n"
+                    f"**🤔DiTambahkan Oleh:** {message.from_user.mention}"
                 )
-                await app.send_photo(
-                    LOG_GROUP_ID,
-                    photo=random.choice(photo),
-                    caption=msg,
-                    reply_markup=InlineKeyboardMarkup(
-                        [
-                            [
-                                InlineKeyboardButton(
-                                    f"😍𝐀ᴅᴅᴇᴅ 𝐁ʏ😍",
-                                    url=f"tg://openmessage?user_id={message.from_user.id}",
-                                )
-                            ]
-                        ]
-                    ),
-                )
-                await add_served_chat(message.chat.id)
+                await app.send_photo(LOG_GROUP_ID, photo=random.choice(photo), caption=msg, reply_markup=InlineKeyboardMarkup([
+                [InlineKeyboardButton(f"Add Me!", url=f"tg://openmessage?user_id={message.from_user.id}")]
+             ]))
                 await userbot.join_chat(f"{username}")
-                oks = await userbot.send_message(LOGGERS, f"/start")
-                Ok = await userbot.send_message(
-                    LOGGERS, f"#{app.username}\n@{app.username}"
-                )
-                await oks.delete()
-                await asyncio.sleep(2)
-                await Ok.delete()
-
     except Exception as e:
         print(f"Error: {e}")
